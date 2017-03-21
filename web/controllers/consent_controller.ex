b140a76e887707f483bace203b8ba8a787ebe701
SimonLab <simon.labondance@gmail.com>
2017-03-21 10:22:51 +0000

    save user in redcap, #29

--- web/controllers/consent_controller.ex
+++ web/controllers/consent_controller.ex
@@ -3,9 +3,25 @@ defmodule ResearchResource.ConsentController do
 
   alias ResearchResource.Redcap
 
-  plug :authenticate_user when action in [:index]
+  plug :authenticate_user when action in [:new, :create]
 
-  def index(conn, _params) do
-    render(conn, "index.html", consent_questions: Redcap.get_instrument_fields("consent"))
+  def new(conn, _params) do
+    render(conn, "new.html", consent_questions: Redcap.get_instrument_fields("consent"))
   end
+
+  def create(conn, %{"consent" => consent}) do
+    user_data = Redcap.user_to_record(conn.assigns.current_user)
+    IO.inspect user_data
+    IO.puts "****************************"
+    # data = Map.merge(consent, user_data)
+    Redcap.save_record(user_data)
+    # get user
+    render(conn, "new.html", consent_questions: Redcap.get_instrument_fields("consent"))
+  #   %{"consent_1" => "Yes", "consent_2" => "No", "consent_3" => "Yes",
+  # "consent_4" => "Yes", "consent_5" => "Yes", "consent_6" => "Yes",
+  # "consent_7" => "Yes"}
+
+
+  end
+
 end
--- web/controllers/redcap.ex
+++ web/controllers/redcap.ex
@@ -12,9 +12,35 @@ defmodule ResearchResource.Redcap do
     filter_fields(res, instrument)
   end
 
+  def user_to_record(user) do
+    %{
+      record_id: user.ttrrid,
+      first_name: user.first_name,
+      last_name: user.last_name,
+      email: user.email
+    }
+  end
+
+  def save_record(data) do
+    {:ok, payload} = Poison.encode(data)
+    IO.puts payload
+    IO.puts "******************"
+    res = HTTPoison.post(@redcap_url, {:form,
+    [token: @redcap_token,
+     content: "record",
+     format: "json",
+     type: "flat",
+     overwriteBehavior: "normal",
+     data: payload
+    ]}, [])
+    IO.inspect res
+  end
+
   defp filter_fields({:ok, res}, instrument) do
     {:ok, data} = Poison.Parser.parse(res.body)
 
     consent_questions = Enum.filter(data, fn(e) -> e["form_name"] == instrument end)
   end
+
+
 end
