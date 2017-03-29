a94ff6357cae89db423f258c36da0b4db7ad612d
Danielwhyte <danwhy@gmail.com>
2017-03-29 17:44:31 +0100

    redirects users to view their consent if already consented #36

--- test/views/consent_view_test.exs
+++ test/views/consent_view_test.exs
@@ -8,4 +8,14 @@ defmodule ResearchResource.ConsentViewTest do
     assert ConsentView.question_name("consent_44", "") == :consent_44_n
     assert ConsentView.question_name("consent_80", "y") == :consent_80_y
   end
+
+  test "get checked yes" do
+    assert ConsentView.get_checked_yes("1") == true
+    assert ConsentView.get_checked_yes("0") == false
+  end
+
+  test "get checked no" do
+    assert ConsentView.get_checked_no("1") == false
+    assert ConsentView.get_checked_no("0") == true
+  end
 end
