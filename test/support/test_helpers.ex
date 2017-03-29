0740a63752016c27fd94f98f62bab29ea66f5364
Sam Houston <shouston3@me.com>
2017-03-29 17:17:17 +0100

    tidying up some of the long lines in test_helpers and more aliases

--- test/support/test_helpers.ex
+++ test/support/test_helpers.ex
@@ -1,5 +1,5 @@
 defmodule ResearchResource.TestHelpers do
-  alias ResearchResource.Repo
+  alias ResearchResource.{Repo, User}
 
   def insert_user(attrs \\ %{}) do
     changes = Map.merge(%{
@@ -11,16 +11,27 @@ defmodule ResearchResource.TestHelpers do
       qualtrics_id: "0"
     }, attrs)
 
-    %ResearchResource.User{}
-    |> ResearchResource.User.registration_changeset(changes)
+    %User{}
+    |> User.registration_changeset(changes)
     |> Repo.insert!()
+  end
+
+  defp add_id_for_username(username) do
+    fn (email, map) ->
+      if username == email do
+        map
+      else
+        %{}
+      end
+    end
   end
 
   def login_user(%{conn: conn} = config, build_conn, assign) do
     if username = config[:login_as] do
+      add_id = add_id_for_username(username)
       user = %{email: username, password: "secret"}
-        |> Map.merge(if username == "nottrrid@nottrrid.com" do %{ttrrid: nil} else %{} end)
-        |> Map.merge(if username == "survey@completed.com" do %{qualtrics_id: "1"} else %{} end)
+        |> Map.merge(add_id.("nottrrid@nottrrid.com", %{ttrrid: nil}))
+        |> Map.merge(add_id.("survey@completed.com", %{qualtrics_id: "1"}))
         |> insert_user
       conn = assign.(build_conn.(), :current_user, user)
       {:ok, conn: conn, user: user}
