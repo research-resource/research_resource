e79fc055e7adf6a43e1efe3c31ec7c38c080c655
Danielwhyte <danwhy@gmail.com>
2017-04-04 11:51:40 +0100

    turns consent question into reusable component #18

--- test/views/consent_view_test.exs
+++ test/views/consent_view_test.exs
@@ -1,21 +1,21 @@
-defmodule ResearchResource.ConsentViewTest do
+defmodule ResearchResource.ComponentViewTest do
   use ResearchResource.ConnCase
-  alias ResearchResource.ConsentView
+  alias ResearchResource.ComponentView
 
   test "creates form names correctly" do
-    assert ConsentView.question_name("consent_1", "y") == :consent_1_y
-    assert ConsentView.question_name("consent_2", "") == :consent_2_n
-    assert ConsentView.question_name("consent_44", "") == :consent_44_n
-    assert ConsentView.question_name("consent_80", "y") == :consent_80_y
+    assert ComponentView.question_name("consent_1", "y") == :consent_1_y
+    assert ComponentView.question_name("consent_2", "") == :consent_2_n
+    assert ComponentView.question_name("consent_44", "") == :consent_44_n
+    assert ComponentView.question_name("consent_80", "y") == :consent_80_y
   end
 
   test "get checked yes" do
-    assert ConsentView.get_checked_yes("1") == true
-    assert ConsentView.get_checked_yes("0") == false
+    assert ComponentView.get_checked_yes("1") == true
+    assert ComponentView.get_checked_yes("0") == false
   end
 
   test "get checked no" do
-    assert ConsentView.get_checked_no("1") == false
-    assert ConsentView.get_checked_no("0") == true
+    assert ComponentView.get_checked_no("1") == false
+    assert ComponentView.get_checked_no("0") == true
   end
 end
--- web/views/consent_view.ex
+++ web/views/consent_view.ex
@@ -1,19 +1,3 @@
 defmodule ResearchResource.ConsentView do
   use ResearchResource.Web, :view
-
-  def get_background_colour(index) when rem(index, 2) == 0, do: "bg-near-white"
-  def get_background_colour(_index), do: "bg-white"
-
-  def question_name(name, req) do
-    case req do
-      "y" ->
-        String.to_atom("#{name}_y")
-      "" ->
-        String.to_atom("#{name}_n")
-    end
-  end
-
-  def get_checked_yes(checked), do: checked == "1"
-
-  def get_checked_no(checked), do: checked == "0"
 end
