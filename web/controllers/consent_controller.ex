4f3ce53b2d2fc2c808e02b5ad4a822db626666ca
Danielwhyte <danwhy@gmail.com>
2017-04-04 16:21:29 +0100

    allows user to input address for saliva kit #18

--- web/controllers/consent_controller.ex
+++ web/controllers/consent_controller.ex
@@ -3,10 +3,11 @@ defmodule ResearchResource.ConsentController do
 
   alias ResearchResource.{Redcap.RedcapHelpers, Qualtrics.QualtricsHelpers, User}
 
-  plug :authenticate_user when action in [:new, :create]
+  plug :authenticate_user when action in [:new, :create, :view]
 
   @redcap_api Application.get_env(:research_resource, :redcap_api)
   @qualtrics_api Application.get_env(:research_resource, :qualtrics_api)
+  @email Application.get_env(:research_resource, :email)
 
   def new(conn, _params) do
     case conn.assigns.current_user.ttrr_consent do
@@ -24,6 +25,7 @@ defmodule ResearchResource.ConsentController do
         conn
         |> create_user_redcap(consent)
         |> update_consent
+        |> send_saliva_email(consent)
         |> create_user_qualtrics
         |> redirect(to: qualtrics_path(conn, :new))
       false ->
@@ -42,6 +44,7 @@ defmodule ResearchResource.ConsentController do
     user_data = RedcapHelpers.user_to_record(conn.assigns.current_user)
 
     consent
+    |> Enum.filter(fn {key, _} -> String.starts_with?(key, "consent") end)
     |> Enum.map(fn {key, val} -> {Regex.replace(~r/_[yn]\b/, key, ""), val} end)
     |> RedcapHelpers.consent_to_record
     |> Map.merge(user_data)
@@ -70,10 +73,14 @@ defmodule ResearchResource.ConsentController do
     Enum.all?(consent, fn {k, v} ->
       case String.last(k) do
         "y" ->
-          v == "Yes"
+          if String.starts_with?(k, "consent") do
+            v == "Yes"
+          else
+            true
+          end
         _ ->
           true
-      end
+        end
     end)
   end
 
@@ -92,5 +99,23 @@ defmodule ResearchResource.ConsentController do
         conn
         |> assign(:current_user, user)
     end
+  end
+
+  def send_saliva_email(conn, user_details) do
+    if (user_details["name"]) do
+      subject = "Saliva Kit Requested"
+      message = "The following user has requested a saliva kit:\n
+      Name: #{user_details["name"]}\n
+      Address:
+      #{user_details["address_1"]}
+      #{user_details["address_2"]}
+      #{user_details["town"]}
+      #{user_details["county"]}
+      #{user_details["postcode"]}"
+
+      ResearchResource.Email.send_email(@email, subject, message)
+      |> ResearchResource.Mailer.deliver_now()
+    end
+    conn
   end
 end
