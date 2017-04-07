0eb5498c372b97a293e4174b1693eb1c10c4ffc5
Danielwhyte <danwhy@gmail.com>
2017-04-07 13:08:54 +0100

    adds terms and conditions page #95

---
+++ test/controllers/ts_and_cs_controller_test.exs
@@ -0,0 +1,8 @@
+defmodule ResearchResource.TsAndCsControllerTest do
+  use ResearchResource.ConnCase
+
+  test "GET /terms_and_conditions", %{conn: conn} do
+    conn = get conn, "/terms_and_conditions"
+    assert html_response(conn, 200) =~ "Terms and Conditions"
+  end
+end
---
+++ web/controllers/ts_and_cs_controller.ex
@@ -0,0 +1,7 @@
+defmodule ResearchResource.TsAndCsController do
+  use ResearchResource.Web, :controller
+
+  def index(conn, _params) do
+    render conn, "index.html"
+  end
+end
